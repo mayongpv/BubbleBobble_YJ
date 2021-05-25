2bddb5e24b9ec48655b9599d79c9a3aba6174e76
mayongpv <82883541+mayongpv@users.noreply.github.com>
2021-05-25 19:36:04 +0900

    점프 & 벽 밖으로 못나가게

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 0.1f;
-
+    new public Rigidbody2D rigidbody2D;
+    new public Collider2D collider2D;
     public Animator animator;
+    public float jumpForce = 300f;
 
     private void Awake()
     {
+        rigidbody2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         Application.targetFrameRate = 60;
     }
@@ -16,7 +19,34 @@ public class Player : MonoBehaviour
         FireBubble();
 
         Move();
+
+        Jump();
+
     }
+
+  
+
+    //공중에서 점프를 막고 싶다.
+
+
+
+    private void Jump()
+    {
+
+        if (rigidbody2D.velocity.y < 0)
+            collider2D.isTrigger = false;
+
+        if (rigidbody2D.velocity.y == 0) 
+        {
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                rigidbody2D.AddForce(new Vector2(0, jumpForce));
+                collider2D.isTrigger = true; //점프할 때 벽을 뚫고 싶다.
+
+            }
+        }
+    }
+
     public GameObject bubble;
     public Transform bubbleSpawnPos;
     private void FireBubble()
@@ -28,6 +58,7 @@ public class Player : MonoBehaviour
         }
     }
 
+    public float minX, maxX;
     private void Move()
     {
 
@@ -38,8 +69,9 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) moveX = -1;
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) moveX = 1;
         Vector3 position = transform.position;
-        position.x = position.x + moveX * speed * Time.deltaTime;
-      
+        position.x = position.x + moveX * speed;
+        position.x = Mathf.Max(minX, position.x);
+        position.x = Mathf.Min(maxX, position.x);
         transform.position = position;
 
         if (animator.GetCurrentAnimatorStateInfo(0).IsName("Attack") == false)
