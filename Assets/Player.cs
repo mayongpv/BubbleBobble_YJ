cd0a52fcdea400ece4172fc772e5855c7b0a8ae6
mayongpv <82883541+mayongpv@users.noreply.github.com>
2021-05-25 18:13:56 +0900

    방향이 바뀌면 Rotate 돌려서 그 방향 보게 하기

---
+++ Assets/Player.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 0.1f;
+
+    public Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        Application.targetFrameRate = 60;
+    }
+    private void Update()
+    {
+        // WASD, W위로, A왼쪽,S아래, D오른쪽
+        float moveX = 0;
+      
+  
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) moveX = -1;
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) moveX = 1;
+        Vector3 position = transform.position;
+        position.x = position.x + moveX * speed * Time.deltaTime;
+      
+        transform.position = position;
+
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Attack") == false)
+        {
+            if (moveX != 0)
+            {//moveX가 양수면 180 로테이션 아니면 0도 로테이션
+                float rotateY = 0;
+                if (moveX > 0)
+                    rotateY = 180;
+
+                var rotation = transform.rotation;
+                rotation.y = rotateY;
+                transform.rotation = rotation;
+                animator.Play("Run");
+            }
+            else
+                animator.Play("Idle");
+        }
+
+
+    }
+}
