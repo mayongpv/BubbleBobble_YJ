9f1cbd5c7470b0217f99533c58f2587421c2e2db
mayongpv <82883541+mayongpv@users.noreply.github.com>
2021-05-26 15:16:37 +0900

    Monster

--- Assets/Monster.cs
+++ Assets/Monster.cs
@@ -1,37 +1,32 @@
-using System.Collections;
+ï»¿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
-    //¾ÕÀ¸·Î ¿òÁ÷¿©¶ó
-    //¹üÀ§¸¦ ÁöÁ¤ÇØ¼­ ±× ¹üÀ§ ¾ÈÀ» ¿Ô´Ù°¬´Ù °Å¸®°Ô ÇÒ ¼öµµ ÀÖÀ½
+    // ì•ìœ¼ë¡œ ì›€ì§ì´ì. 
+    // ë²½ì„ ë§Œë‚˜ë©´ ë°©í–¥ ì „í™˜.
 
     new Rigidbody2D rigidbody2D;
-
     private void Awake()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
-
     public float speed = 0.1f;
-
     private void FixedUpdate()
     {
         var pos = rigidbody2D.position;
         pos.x += speed * transform.forward.z;
         rigidbody2D.position = pos;
     }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         var rotation = transform.rotation;
-        if (rotation.y == 0) 
-        rotation.y = 180;
+        if (rotation.y == 0)
+            rotation.y = 180;
         else
             rotation.y = 0;
 
         transform.rotation = rotation;
     }
-
 }
